c06727d86a98411188ac54020bd54efae8dd0588
Nikola Prokopić <nikola@prokopic.rs>
2021-04-20 16:50:18 +0200

    Fix imports

--- pkg/filter/filter.go
+++ pkg/filter/filter.go
@@ -5,15 +5,15 @@ import (
 
 	"github.com/giantswarm/apiextensions/v3/pkg/label"
 	"github.com/giantswarm/microerror"
-	"k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/cluster-api/api/v1alpha3"
-	"sigs.k8s.io/cluster-api/util"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	capi "sigs.k8s.io/cluster-api/api/v1alpha3"
+	capiutil "sigs.k8s.io/cluster-api/util"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/giantswarm/azure-admission-controller/pkg/release"
 )
 
-func IsCRProcessed(ctx context.Context, ctrlClient client.Client, objectMeta v1.Object) (bool, error) {
+func IsCRProcessed(ctx context.Context, ctrlClient client.Client, objectMeta metav1.Object) (bool, error) {
 	// Try to get release label from the CR
 	releaseVersionLabel := objectMeta.GetLabels()[label.ReleaseVersion]
 	if releaseVersionLabel == "" {
@@ -21,7 +21,7 @@ func IsCRProcessed(ctx context.Context, ctrlClient client.Client, objectMeta v1.
 		// Cluster CR
 
 		// first let's try CAPI cluster name label
-		clusterName := objectMeta.GetLabels()[v1alpha3.ClusterLabelName]
+		clusterName := objectMeta.GetLabels()[capi.ClusterLabelName]
 		if clusterName == "" {
 			// CAPI cluster name label not found, now let's try GS cluster ID label
 			clusterID := objectMeta.GetLabels()[label.Cluster]
@@ -36,7 +36,7 @@ func IsCRProcessed(ctx context.Context, ctrlClient client.Client, objectMeta v1.
 
 		// now get the owner cluster by name, we will try to check if it has
 		// release label
-		cluster, err := util.GetClusterByName(ctx, ctrlClient, objectMeta.GetNamespace(), clusterName)
+		cluster, err := capiutil.GetClusterByName(ctx, ctrlClient, objectMeta.GetNamespace(), clusterName)
 		if err != nil {
 			return false, microerror.Mask(err)
 		}
