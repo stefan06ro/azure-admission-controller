e3fda993e023e7ffd1c9a365b1e7559081f8042b
Nikola Prokopić <nikola@prokopic.rs>
2021-04-28 15:02:21 +0200

    Merge azurecluster.Decoder into WebhookHandler

--- pkg/azurecluster/webhook_handler.go
+++ pkg/azurecluster/webhook_handler.go
@@ -3,12 +3,16 @@ package azurecluster
 import (
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	capz "sigs.k8s.io/cluster-api-provider-azure/api/v1alpha3"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/giantswarm/azure-admission-controller/internal/errors"
+	"github.com/giantswarm/azure-admission-controller/pkg/validator"
 )
 
 type WebhookHandler struct {
-	Decoder
-
 	baseDomain string
 	ctrlClient client.Client
 	location   string
@@ -37,8 +41,6 @@ func NewWebhookHandler(config WebhookHandlerConfig) (*WebhookHandler, error) {
 	}
 
 	v := &WebhookHandler{
-		Decoder: Decoder{},
-
 		baseDomain: config.BaseDomain,
 		ctrlClient: config.CtrlClient,
 		location:   config.Location,
@@ -54,4 +56,13 @@ func (h *WebhookHandler) Log(keyVals ...interface{}) {
 
 func (h *WebhookHandler) Resource() string {
 	return "azurecluster"
+}
+
+func (h *WebhookHandler) Decode(rawObject runtime.RawExtension) (metav1.ObjectMetaAccessor, error) {
+	azureClusterCR := &capz.AzureCluster{}
+	if _, _, err := validator.Deserializer.Decode(rawObject.Raw, nil, azureClusterCR); err != nil {
+		return nil, microerror.Maskf(errors.ParsingFailedError, "unable to parse AzureCluster CR: %v", err)
+	}
+
+	return azureClusterCR, nil
 }
